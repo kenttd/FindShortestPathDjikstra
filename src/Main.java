bcf4a59b78078c231cc87bd58f482ccf117d61f3
kenttdd <kenttravisdonovan@gmail.com>
2023-09-16 18:35:19 +0700

    initial commit
    sudah hampir semua, ada djikstra, kurang vizualize

--- src/Main.java
+++ src/Main.java
@@ -6,92 +6,175 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.Point;
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.plaf.nimbus.NimbusLookAndFeel;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.plaf.nimbus.NimbusLookAndFeel;
+import javax.swing.SwingUtilities;
 
-public class Main extends JFrame{
-	private JPanel mainPanel;
-    private JPanel leftPanel;
-    private JButton addButton;
-    private JButton connectButton;
-    private JButton startButton;
-    private JTextField textField;
-    private boolean isAddToggled = false;
-    private boolean isConnectToggled = false;
-    private int circleCounter = 0;
-    private Point firstPoint = null;
-    private Point secondPoint = null;
-    private boolean isFirstPointSelected = false;
-    private List<Point> circleCenters = new ArrayList<>();
-    private JComboBox<String> circleDropdown;
-    private List<vertex> vertices = new ArrayList<>();
-
-    public Main() {
-        setTitle("Graph-like GUI");
-        setSize(800, 600);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        mainPanel = new JPanel(new BorderLayout());
-        add(mainPanel);
-
-        setupUI();
-    }
+public class Main{
+	static JFrame frame;
+	static JButton addButton;
+	static JButton connectButton;
+	static JButton startButton;
+	static boolean onToggleAdd = false;
+	static boolean onToggleConnect = false;
+	static JComboBox<String> choice;
+	static JPanel display;
+	static List<vertex> vertices = new ArrayList<>();
+	static private int vertexCounter = 0;
+	static vertex selectedVertex = null;
+	static boolean isDrawingLine = false;
+	static Point mousePosition;
+	static final int editMode = 1;
+	static final int resultMode = 2;
+	static int currentMode;
+	static ArrayList<Character> unvisited = new ArrayList<>();
+	static ArrayList<Character> visited = new ArrayList<>();
 	public static void main(String[] args) {
-		SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                new Main().setVisible(true);
-            }
-        });
+		setup();
+		eventSetup();
 	}
-	private void setupUI() {
-	    // Left Panel
-	    leftPanel = new JPanel() {
+	
+	public static void setup() {
+		currentMode = editMode;
+		frame = new JFrame();
+		frame.setSize(1200,800);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLayout(null);
+		frame.setResizable(false);
+		addButton = new JButton("Add");
+		connectButton = new JButton("Connect");
+		startButton = new JButton("Start");
+		addButton.setBounds(1050, 100, 100, 100);
+		connectButton.setBounds(1050, 250, 100, 100);
+		startButton.setBounds(1050, 440, 100, 100);
+		frame.add(addButton);
+		frame.add(connectButton);
+		frame.add(startButton);
+		choice = new JComboBox<String>();
+		choice.setBounds(1050, 400, 100, 50);
+		frame.add(choice);
+		display = new JPanel() {
+		    @Override
+		    protected void paintComponent(Graphics g) {
+		        super.paintComponent(g);
+		        if(currentMode==editMode) {
+		        	for (vertex vertex : vertices) {
+			            vertex.draw(g);
+			        }
+			        // Drawing established connections
+			        for (vertex v : vertices) {
+			            for (Map.Entry<vertex, Integer> entry : v.connection.entrySet()) {
+			                vertex connectedVertex = entry.getKey();
+			                // Check to ensure drawing only once
+			                if (v.name < connectedVertex.name) {
+			                    int weight = entry.getValue();
+			                    g.setColor(Color.BLACK);
+			                    g.drawLine(v.position.x, v.position.y, connectedVertex.position.x, connectedVertex.position.y);
+			                    // Calculate the midpoint
+			                    int midX = (v.position.x + connectedVertex.position.x) / 2;
+			                    int midY = (v.position.y + connectedVertex.position.y) / 2;
+			                    // Display the weight slightly above the midpoint
+			                    g.setColor(Color.RED);
+			                    g.drawString(String.valueOf(weight), midX, midY - 10);
+			                }
+			            }
+			        }
+
+			        if (isDrawingLine && selectedVertex != null && mousePosition != null) {
+			            g.setColor(Color.BLACK);
+			            g.drawLine(selectedVertex.position.x, selectedVertex.position.y, mousePosition.x, mousePosition.y);
+			        }
+		        }else if(currentMode==resultMode) {
+		        	
+		        }
+		    }
+		};
+		display.setBounds(30,30,1000,700);
+		display.setBackground(Color.white);
+		frame.add(display);
+		frame.repaint();
+		frame.setVisible(true);
+	}
+	
+	private static void eventSetup() {
+		display.addMouseListener(new MouseAdapter() {
+		    @Override
+		    public void mouseClicked(MouseEvent e) {
+		    	
+		        System.out.println("clicked display");
+		        
+		        if (onToggleAdd) {
+		            Point clickPosition = e.getPoint();
+		            char name = (char) ('A' + vertexCounter);
+		            vertex newVertex = new vertex(clickPosition, 20, name); // You can change the radius and color as per your liking
+		            vertices.add(newVertex);
+		            unvisited.add(name);
+		            choice.addItem(String.valueOf(name));
+		            display.repaint(); // To refresh and display the new circle
+		            vertexCounter++;
+		        } else if (isDrawingLine && selectedVertex != null) {
+		            for (vertex v : vertices) {
+		                if (v.contains(e.getPoint())) {
+		                	int index1 = findVertexIndex(selectedVertex.name);
+		                	int index2 = findVertexIndex(v.name);
+		                	int computedDistance = vertices.get(index1).distanceTo(v);
+		                	vertices.get(index1).connection.put(v, computedDistance);
+		                	vertices.get(index2).connection.put(selectedVertex, computedDistance);
+		                	System.out.println("Connecting " + selectedVertex.name + " to " + v.name + " with distance: " + vertices.get(index1).distanceTo(v));
+		                    // Code to draw a line between selectedVertex and v
+		                    // Note: You might want to store these connections in a data structure for future use or display
+		                	for (vertex vert : vertices) {
+		                        for (Map.Entry<vertex, Integer> entry : vert.connection.entrySet()) {
+		                            System.out.println("Distance between " + vert.name + " and " + entry.getKey().name + " is: " + entry.getValue());
+		                        }
+		                    }
+		                    break;
+		                }
+		            }
+		            isDrawingLine = false;
+		            selectedVertex = null;
+		            display.repaint();
+		        } else {
+		            for (vertex v : vertices) {
+		                if (v.contains(e.getPoint())) {
+		                    selectedVertex = v;
+		                    isDrawingLine = true;
+		                    break;
+		                }
+		            }
+		        }
+		        display.repaint();
+		    }
+		});
+		
+		display.addMouseMotionListener(new MouseAdapter() {
 	        @Override
-	        protected void paintComponent(Graphics g) {
-	            super.paintComponent(g);
-	            for (vertex vertex : vertices) {
-	                g.setColor(Color.BLACK);
-	                g.fillOval(vertex.center.x - vertex.radius, vertex.center.y - vertex.radius, 2 * vertex.radius, 2 * vertex.radius);
-	                g.setColor(Color.WHITE);
-	                g.drawString(vertex.label, vertex.center.x - 4, vertex.center.y + 4);
+	        public void mouseMoved(MouseEvent e) {
+	            if (isDrawingLine && selectedVertex != null) {
+	                mousePosition = e.getPoint();
+	                display.repaint();
 	            }
 	        }
-	    };
-
-	    leftPanel.setBackground(Color.WHITE);
-	    mainPanel.add(leftPanel, BorderLayout.CENTER);
-
-	    // Right Panel
-	    JPanel rightPanel = new JPanel();
-	    rightPanel.setLayout(new BoxLayout(rightPanel, BoxLayout.Y_AXIS));
-	    mainPanel.add(rightPanel, BorderLayout.EAST);
-
-	    addButton = new JButton("Add");
-	    connectButton = new JButton("Connect");
-	    startButton = new JButton("Start");
-	    
-	    // Panel for Start button and dropdown
-	    JPanel startAndDropdownPanel = new JPanel(new FlowLayout(FlowLayout.LEFT, 5, 5)); // 5 pixel gaps
-	    startAndDropdownPanel.add(startButton);
-
-	    circleDropdown = new JComboBox<>();
-	    startAndDropdownPanel.add(circleDropdown);
-
-	    rightPanel.add(addButton);
-	    rightPanel.add(connectButton);
-	    rightPanel.add(startAndDropdownPanel);
-
-	    setupListeners();
-	}
-
-    
-    private void setupListeners() {
-    	addButton.addActionListener(new ActionListener() {
+	    });
+		
+		addButton.addActionListener(new ActionListener() {
     	    @Override
     	    public void actionPerformed(ActionEvent e) {
-    	        isAddToggled = !isAddToggled;
-    	        if (isAddToggled) { // If Add is toggled on, toggle off Connect
-    	            isConnectToggled = false;
+    	    	currentMode = editMode;
+    	    	onToggleAdd = !onToggleAdd;
+    	        if (onToggleAdd) { // If Add is toggled on, toggle off Connect
+    	        	onToggleConnect = false;
     	        }
     	    }
     	});
@@ -99,70 +182,53 @@ public class Main extends JFrame{
     	connectButton.addActionListener(new ActionListener() {
     	    @Override
     	    public void actionPerformed(ActionEvent e) {
-    	        isConnectToggled = !isConnectToggled;
-    	        if (isConnectToggled) { // If Connect is toggled on, toggle off Add
-    	            isAddToggled = false;
+    	    	currentMode = editMode;
+    	    	onToggleConnect = !onToggleConnect;
+    	        if (onToggleConnect) { // If Connect is toggled on, toggle off Add
+    	        	onToggleAdd = false;
     	        }
     	    }
     	});
-
-
-        leftPanel.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-            	if (isAddToggled) {
-            	    circleCenters.add(e.getPoint());
-            	    String label = String.valueOf((char) ('a' + circleCounter));
-            	    vertices.add(new vertex(e.getPoint(), 15, label));  // 15 is the radius
-            	    circleCounter++;
-
-            	    String nextLabel = String.valueOf((char) ('A' + circleCenters.size() - 1));
-            	    circleDropdown.addItem(nextLabel);
-            	    leftPanel.repaint();
-            	}
-            	else if (isConnectToggled) {
-                    Graphics g = leftPanel.getGraphics();
-                    Point circleCenter = getCircleCenterFromPoint(e.getPoint());
-                    if (circleCenter != null) {  // This checks if the point is inside a circle
-                        if (!isFirstPointSelected) {
-                            firstPoint = circleCenter;
-                            isFirstPointSelected = true;
-                        } else {
-                            secondPoint = circleCenter;
-                            
-                            // Drawing the line between two points
-                            g.setColor(Color.BLACK);
-                            g.drawLine(firstPoint.x, firstPoint.y, secondPoint.x, secondPoint.y);
-                            
-                            // Calculate the midpoint of the line
-                            int midX = (firstPoint.x + secondPoint.x) / 2;
-                            int midY = (firstPoint.y + secondPoint.y) / 2;
-                            
-                            // Calculate the weight (distance)
-                            double distance = firstPoint.distance(secondPoint);
-                            String weight = String.format("%.2f", distance); // Display with 2 decimal places
-                            
-                            // Draw the weight string either above or below the midpoint
-                            g.drawString(weight, midX, midY - 10);  // 10 pixels above the midpoint. Adjust as necessary.
-                            
-                            isFirstPointSelected = false; // Reset for the next set of points
-                            firstPoint = null;
-                            secondPoint = null;
-                        }
-                    }
-                }
-            }
-        });
-    }
-
-    private Point getCircleCenterFromPoint(Point point) {
-        int effectiveRadius = 50; // Slightly larger than circle's actual radius to ensure click recognition
-        for (Point center : circleCenters) {
-            if (point.distance(center) <= effectiveRadius) {
-                return center;  // Return the center of the circle clicked within
-            }
-        }
-        return null;
-    }
-
+    	
+    	startButton.addActionListener(new ActionListener() {
+    	    @Override
+    	    public void actionPerformed(ActionEvent e) {
+    	    	currentMode = resultMode;
+    	    	djikstra(((String) choice.getSelectedItem()).charAt(0));
+    	    }
+    	});
+	}
+	
+	private static void djikstra(char start) {
+		char current = start;
+		int index = findVertexIndex(start);
+		if(index!=-1)vertices.get(index).shortest=0;
+		while(!unvisited.isEmpty()) {
+			vertex visiting = getLowest();
+			for (Map.Entry<vertex, Integer> entry : vertices.get(findVertexIndex(visiting.name)).connection.entrySet()) {
+				if(vertices.get(findVertexIndex(entry.getKey().name)).shortest>visiting.shortest+entry.getValue()) {
+					vertices.get(findVertexIndex(entry.getKey().name)).shortest=visiting.shortest+entry.getValue();
+					vertices.get(findVertexIndex(entry.getKey().name)).prev=visiting.name;
+				}
+			}
+			unvisited.remove((Character)visiting.name);
+			visited.add(visiting.name);
+		}
+		System.out.println(vertices);
+	}
+	
+	private static vertex getLowest() {
+	    return vertices.stream()
+	    		.filter(g -> !visited.contains(g.name))  // Only consider graphs with valid names
+	            .min(Comparator.comparing(g -> g.shortest)) // Find the graph with the minimum shortest value
+	            .orElse(null);  // Return null if no suitable graph is found
+	}
+	
+	private static int findVertexIndex(char name) {
+		for(int i = 0; i<vertices.size(); i++) {
+			if(vertices.get(i).name==name)return i;
+		}
+		return -1;
+	}
 }
+
--- src/vertex.java
+++ src/vertex.java
@@ -1,14 +1,46 @@
+import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.Point;
+import java.util.HashMap;
+import java.util.Map;
 
 public class vertex {
-	Point center;
-    int radius;
-    String label;
+	Point position;
+	int radius;
+	char name;
+	HashMap<vertex, Integer> connection = new HashMap<>();
+	char prev;
+	Integer shortest = Integer.MAX_VALUE;
+	public vertex (Point position, int radius, char name) {
+		this.position = position;
+		this.radius = radius;
+		this.name = name;
+	}
+	
+	public void draw(Graphics g) {
+	    g.setColor(Color.black);
+	    g.fillOval(position.x - radius, position.y - radius, 2 * radius, 2 * radius);
 
-    public vertex(Point center, int radius, String label) {
-        this.center = center;
-        this.radius = radius;
-        this.label = label;
-    }
-    
+	    // Drawing the name inside the circle
+	    g.setColor(Color.white);  // Setting the text color to white for contrast
+	    int stringWidth = g.getFontMetrics().stringWidth(String.valueOf(name));
+	    int stringHeight = g.getFontMetrics().getAscent();
+	    g.drawString(String.valueOf(name), position.x - stringWidth / 2, position.y + stringHeight / 2);
+	}
+	
+	public boolean contains(Point p) {
+	    double distance = position.distance(p);
+	    return distance <= radius;
+	}
+	
+	public int distanceTo(vertex other) {
+	    int dx = this.position.x - other.position.x;
+	    int dy = this.position.y - other.position.y;
+	    return (int) Math.sqrt(dx*dx + dy*dy);
+	}
+	
+	@Override
+	public String toString() {
+		return "graph [name=" + name + ", prev=" + prev + ", shortest=" + shortest + "]";
+	}
 }
